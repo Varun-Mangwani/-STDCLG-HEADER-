dea16b8772c9799f74d581cabbdfcaf1311a3755
Varun Mangwani <vpm.media.business@gmail.com>
2026-09-04 15:40:29 +0000

    Refactor Mode_of_array function and improve formatting in Maths.hpp

--- Maths/Math.hpp
+++ Maths/Math.hpp
@@ -30,9 +30,9 @@ int Reverse_of_digits(int value)
 
 double mean_Of_Arrays(int *arr, int size)
 {
-    
+
     int sum = 0;
-    for (int i = 0; i <= size-1 ; i++)
+    for (int i = 0; i <= size - 1; i++)
     {
         sum = sum + arr[i];
     }
@@ -41,8 +41,39 @@ double mean_Of_Arrays(int *arr, int size)
     return avg;
 }
 
-int Mode_of_array(int *array,int size) {
-    int Rptd,count=0;
-    
+int Mode_of_array(int *array, int size)
+{
+    int val, maxval, Rptd, maxcnt;
 
+    for (int i = 0; i <= size - 1; i++)
+    {
+        if (array[i] > array[i + 1])
+        {
+            int tmp = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = tmp;
+        }
+    }
+
+    for (int i = 0; i <= size - 1; i++)
+    {
+        if (i == 0)
+        {
+            val = array[i];
+            Rptd = 0;
+        }
+
+        if (val == array[i] && i != 0)
+        {
+            Rptd++;
+        }
+        else
+        {
+            maxval = val;
+            maxcnt = Rptd > maxcnt ? Rptd : maxcnt;
+            val = array[i];
+            Rptd = 0;
+        }
+    }
+    return maxval;
 }
