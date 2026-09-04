ba4190ba0082026e165c3abafdfd3d68cafd14e1
Varun Mangwani <vpm.media.business@gmail.com>
2026-09-04 15:22:57 +0000

    Refactor Maths functions and update Main.cpp structure

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,22 @@
 #include "Maths/Math.hpp"
 
 int main() {
-    int sum = Sum_Of_Digits(12345);
-    std::cout << sum << std::endl;
+
+    // Basic Sum Of Digits
+    // int sum = Sum_Of_Digits(12345);
+    // std::cout << sum << std::endl;
+
+    // Basic Reverse Of Digits
+    // int rev = Reverse_of_digits(1454);
+    // std::cout << rev << std::endl;
+
+    // Mean of arrays
+    const int arraySize = 10; 
+    int array[arraySize] = {1,2,3,4,5,6,7,8,9,10};
+   
+
+    double Mean = mean_Of_Arrays(array,arraySize);
+    std::cout << Mean << std::endl;
+
     return 0;
 }
--- Maths/Math.hpp
+++ Maths/Math.hpp
@@ -1,14 +1,48 @@
-#include<iostream>
-#include<cmath>
+#include <iostream>
+#include <cmath>
 
-//Only Functions In This Class For Maths Calculations 
+// Only Functions In This Class For Maths Calculations
 
-int Sum_Of_Digits(int value) {
-    int dgt,sum = 0;
-    while(value != 0) {
-        dgt = value%10;
+int Sum_Of_Digits(int value)
+{
+    int dgt, sum = 0;
+    while (value != 0)
+    {
+        dgt = value % 10;
         sum = sum + dgt;
         value /= 10;
     }
     return sum;
+}
+
+int Reverse_of_digits(int value)
+{
+    int dgt, rev = 0;
+
+    while (value != 0)
+    {
+        dgt = value % 10;
+        rev = 10 * rev + dgt;
+        value /= 10;
+    }
+    return rev;
+}
+
+double mean_Of_Arrays(int *arr, int size)
+{
+    
+    int sum = 0;
+    for (int i = 0; i <= size-1 ; i++)
+    {
+        sum = sum + arr[i];
+    }
+    double avg = (double)sum / size;
+
+    return avg;
+}
+
+int Mode_of_array(int *array,int size) {
+    int Rptd,count=0;
+    
+
 }
