e285cda48dc9b4974ee13ec09dc6e0fa57c78382
Varun Mangwani <vpm.media.business@gmail.com>
2026-09-04 15:46:19 +0000

    Update Main.cpp to use Mode_of_array function and modify array initialization

--- Main.cpp
+++ Main.cpp
@@ -13,11 +13,12 @@ int main() {
 
     // Mean of arrays
     const int arraySize = 10; 
-    int array[arraySize] = {1,2,3,4,5,6,7,8,9,10};
+    int array[arraySize] = {1,1,1,1,1,1,2,3,4,5};
    
-
-    double Mean = mean_Of_Arrays(array,arraySize);
-    std::cout << Mean << std::endl;
+    int Mode = Mode_of_array(array,arraySize);
+    std::cout << Mode << std::endl;
+    // double Mean = mean_Of_Arrays(array,arraySize);
+    // std::cout << Mean << std::endl;
 
     return 0;
 }
--- Maths/Math.hpp
+++ Maths/Math.hpp
@@ -41,39 +41,39 @@ double mean_Of_Arrays(int *arr, int size)
     return avg;
 }
 
-int Mode_of_array(int *array, int size)
-{
-    int val, maxval, Rptd, maxcnt;
+// int Mode_of_array(int *array, int size)
+// {
+//     int val, maxval, Rptd, maxcnt;
 
-    for (int i = 0; i <= size - 1; i++)
-    {
-        if (array[i] > array[i + 1])
-        {
-            int tmp = array[i];
-            array[i] = array[i + 1];
-            array[i + 1] = tmp;
-        }
-    }
+//     for (int i = 0; i <= size - 1; i++)
+//     {
+//         if (array[i] > array[i + 1])
+//         {
+//             int tmp = array[i];
+//             array[i] = array[i + 1];
+//             array[i + 1] = tmp;
+//         }
+//     }
 
-    for (int i = 0; i <= size - 1; i++)
-    {
-        if (i == 0)
-        {
-            val = array[i];
-            Rptd = 0;
-        }
+//     for (int i = 0; i <= size - 1; i++)
+//     {
+//         if (i == 0)
+//         {
+//             val = array[i];
+//             Rptd = 0;
+//         }
 
-        if (val == array[i] && i != 0)
-        {
-            Rptd++;
-        }
-        else
-        {
-            maxval = val;
-            maxcnt = Rptd > maxcnt ? Rptd : maxcnt;
-            val = array[i];
-            Rptd = 0;
-        }
-    }
-    return maxval;
-}
+//         if (val == array[i] && i != 0)
+//         {
+//             Rptd++;
+//         }
+//         else
+//         {
+//             maxval = val;
+//             maxcnt = Rptd > maxcnt ? Rptd : maxcnt;
+//             val = array[i];
+//             Rptd = 0;
+//         }
+//     }
+//     return maxval;
+// }
